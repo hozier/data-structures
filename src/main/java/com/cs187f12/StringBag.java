7236b35636ff5aedd9944fd61a600d9d7d755e32
Nijito Tanaka <ihp.hozier@me.com>
2014-08-27 17:06:24 -0400

    cs187f12 package commit.

--- src/main/java/com/cs187f12/StringBag.java
+++ src/main/java/com/cs187f12/StringBag.java
@@ -1,27 +1,35 @@
 package com.cs187f12;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * Created by MDCCLXXVI on 8/27/14.
  */
 public class StringBag {
     StringBag(){
-        bookkeeper = new ArrayList<String>();
+        bookkeeper = new LinkedList<String>();
     }
 
     public String remove(int x){
-        String retrieve = "Error: Invalid index. ";
+        ListIterator<String> iter = bookkeeper.listIterator();
+        String retrieve = "Error: Invalid index. "; 
+
         try {
-            retrieve= bookkeeper.get(x);
-            bookkeeper.remove(x);
+            for (int i = 0; i < bookkeeper.size(); i++) {
+                retrieve = iter.next();
+                if(i == x){break;}
+            }
+            iter.remove();
         }
         catch (ArrayIndexOutOfBoundsException exception){
             exception.printStackTrace();
         }
         return retrieve;
     }
+
 
     List<String> bookkeeper;
 }
